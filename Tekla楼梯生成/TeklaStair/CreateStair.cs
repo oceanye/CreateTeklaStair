057074bfaf903a64c7044b479b2442cdfc7517bc
3080Ti <ocean.yef@yefan.me>
2023-01-16 15:56:17 +0800

    完成钢筋导入

--- Tekla楼梯生成/TeklaStair/CreateStair.cs
+++ Tekla楼梯生成/TeklaStair/CreateStair.cs
@@ -153,36 +153,27 @@ namespace TeklaStair
                 {
                     string point_info = Platetable.Rows[x].ItemArray[2].ToString().Replace('(', ' ').Replace(')', ' ');
                     string plate_thick = Platetable.Rows[x].ItemArray[3].ToString();
+                    string plate_name = Platetable.Rows[x].ItemArray[6].ToString();
 
-                    int np = Convert.ToInt32(point_info.Split('*')[0]);
-
-
-
-
-                        List<string> point_List = new List<string>();
-                        point_List = point_info.Split('*').ToList();
-                        if (Convert.ToDouble(point_List[0]) > 2)
-                        {
-                            point_List.RemoveAt(0);
-                            ContourPlate CP = new ContourPlate();
+                    //int np = Convert.ToInt32(point_info.Split('*')[0]);
 
 
-                            foreach (string point_i in point_List)
-                            {
-                                Tekla.Structures.Geometry3d.Point Pt_i = new Tekla.Structures.Geometry3d.Point(Convert.ToDouble(point_i.Split(',')[0]), Convert.ToDouble(point_i.Split(',')[1]), Convert.ToDouble(point_i.Split(',')[2]));
-                                ContourPoint point_CP_i = new ContourPoint(Pt_i, null);
-                                CP.AddContourPoint(point_CP_i);
+                    //Beam Beam = new Beam(new Point(5000, 7000, 0), new Point(6000, 7000, 0));
+                    //Beam.Profile.ProfileString = "250*250";
+                    //Beam.Material.MaterialString = "K40-1";
+                    //Beam.Finish = "PAINT";
+                    //Beam.Insert();
 
-                            }
-
-                            CP.Profile.ProfileString = "PL" + plate_thick;
-                            CP.Material.MaterialString = "Q235B";
-                            CP.Insert();
-                            myModelPlate.CommitChanges();
-                        }
-
-                    
-                    
+                    //double MinimumY = Beam.GetSolid().MinimumPoint.Y;
+                    //double MinimumX = Beam.GetSolid().MinimumPoint.X;
+                    //double MinimumZ = Beam.GetSolid().MinimumPoint.Z;
+                    //double MaximumY = Beam.GetSolid().MaximumPoint.Y;
+                    //double MaximumX = Beam.GetSolid().MaximumPoint.X;
+                    //double MaximumZ = Beam.GetSolid().MaximumPoint.Z;
+                    if(plate_name == "构造钢筋")
+                        Generate_Rebar(point_info, plate_thick,myModelPlate);
+                    else
+                        Generate_Plate(point_info, plate_thick, myModelPlate);
                 }
                     
 
@@ -200,11 +191,81 @@ namespace TeklaStair
                 conn.Close();
 
 
+                
+
+               
+
+
+
             }
 
 
 
         }
+
+        private static void Generate_Rebar(string point_info,string plate_thick, Model myModelPlate)
+        {
+            string ps = point_info.Split('*')[1];
+            string pe = point_info.Split('*')[2];
+
+            Polygon Polygon = new Polygon();
+            Polygon.Points.Add(new Point(Convert.ToDouble(ps.Split(',')[0]), Convert.ToDouble(ps.Split(',')[1]),
+                Convert.ToDouble(ps.Split(',')[2])));
+            Polygon.Points.Add(new Point(Convert.ToDouble(pe.Split(',')[0]), Convert.ToDouble(pe.Split(',')[1]),
+                Convert.ToDouble(pe.Split(',')[2])));
+
+            SingleRebar SingleRebar = new SingleRebar();
+            SingleRebar.Polygon = Polygon;
+            //SingleRebar.Father = Beam;
+            SingleRebar.Name = "SingleRebar";
+            SingleRebar.Class = 9;
+            SingleRebar.Size = plate_thick;
+            SingleRebar.NumberingSeries.StartNumber = 0;
+            SingleRebar.NumberingSeries.Prefix = "Single";
+            SingleRebar.Grade = "HRB400";
+            //SingleRebar.OnPlaneOffsets = new ArrayList();
+            //SingleRebar.OnPlaneOffsets.Add(25.00);
+            //SingleRebar.StartHook.Angle = -90;
+            //SingleRebar.StartHook.Length = 10;
+            //SingleRebar.StartHook.Radius = 10;
+            //SingleRebar.StartHook.Shape = RebarHookData.RebarHookShapeEnum.CUSTOM_HOOK;
+            //SingleRebar.EndHook.Angle = 90;
+            //SingleRebar.EndHook.Length = 10;
+            //SingleRebar.EndHook.Radius = 10;
+            //SingleRebar.EndHook.Shape = RebarHookData.RebarHookShapeEnum.CUSTOM_HOOK;
+
+            SingleRebar.Insert();
+            myModelPlate.CommitChanges();
+        }
+
+        private static void Generate_Plate(string point_info, string plate_thick, Model myModelPlate)
+        {
+            List<string> point_List = new List<string>();
+            point_List = point_info.Split('*').ToList();
+            if (Convert.ToDouble(point_List[0]) > 2)
+            {
+                point_List.RemoveAt(0);
+                ContourPlate CP = new ContourPlate();
+
+
+                foreach (string point_i in point_List)
+                {
+                    Tekla.Structures.Geometry3d.Point Pt_i = new Tekla.Structures.Geometry3d.Point(
+                        Convert.ToDouble(point_i.Split(',')[0]), Convert.ToDouble(point_i.Split(',')[1]),
+                        Convert.ToDouble(point_i.Split(',')[2]));
+                    ContourPoint point_CP_i = new ContourPoint(Pt_i, null);
+                    CP.AddContourPoint(point_CP_i);
+                }
+
+                CP.Profile.ProfileString = "PL" + plate_thick;
+                CP.Material.MaterialString = "Q235B";
+                CP.Insert();
+                myModelPlate.CommitChanges();
+            }
+        }
+
+        
+
         /// <summary>
         /// 获得数据库数据
         /// </summary>
