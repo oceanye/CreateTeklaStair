cd096aeefd80c14a550756f46233234bb05844d7
3080Ti <ocean.yef@yefan.me>
2023-03-06 13:41:18 +0800

    V2.0 1.折板生成 2.界面加入截面选择

--- Tekla楼梯生成/TeklaStair/CreateStair.Designer.cs
+++ Tekla楼梯生成/TeklaStair/CreateStair.Designer.cs
@@ -30,34 +30,91 @@ namespace TeklaStair
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.Cmb_Section = new System.Windows.Forms.ComboBox();
+            this.beam_section_text = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Cmb_stair_type = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button1.Location = new System.Drawing.Point(96, 77);
+            this.button1.Location = new System.Drawing.Point(89, 123);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(127, 41);
+            this.button1.Size = new System.Drawing.Size(127, 29);
             this.button1.TabIndex = 0;
             this.button1.Text = "生成楼梯";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Cmb_Section
+            // 
+            this.Cmb_Section.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Cmb_Section.FormattingEnabled = true;
+            this.Cmb_Section.ItemHeight = 19;
+            this.Cmb_Section.Location = new System.Drawing.Point(133, 62);
+            this.Cmb_Section.Name = "Cmb_Section";
+            this.Cmb_Section.Size = new System.Drawing.Size(172, 27);
+            this.Cmb_Section.TabIndex = 1;
+            // 
+            // beam_section_text
+            // 
+            this.beam_section_text.AutoSize = true;
+            this.beam_section_text.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.beam_section_text.Location = new System.Drawing.Point(23, 27);
+            this.beam_section_text.Name = "beam_section_text";
+            this.beam_section_text.Size = new System.Drawing.Size(104, 16);
+            this.beam_section_text.TabIndex = 2;
+            this.beam_section_text.Text = "楼梯类型选择";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(23, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "调整梯梁截面";
+            // 
+            // Cmb_stair_type
+            // 
+            this.Cmb_stair_type.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Cmb_stair_type.FormattingEnabled = true;
+            this.Cmb_stair_type.ItemHeight = 19;
+            this.Cmb_stair_type.Items.AddRange(new object[] {
+            "通用钢楼梯(型钢)",
+            "板式梯梁钢楼梯"});
+            this.Cmb_stair_type.Location = new System.Drawing.Point(133, 22);
+            this.Cmb_stair_type.Name = "Cmb_stair_type";
+            this.Cmb_stair_type.Size = new System.Drawing.Size(172, 27);
+            this.Cmb_stair_type.TabIndex = 4;
+            this.Cmb_stair_type.SelectedIndexChanged += new System.EventHandler(this.Cmb_stair_type_SelectedIndexChanged);
             // 
             // CreateStair
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(330, 180);
+            this.Controls.Add(this.Cmb_stair_type);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.beam_section_text);
+            this.Controls.Add(this.Cmb_Section);
             this.Controls.Add(this.button1);
             this.Name = "CreateStair";
-            this.Text = "生成楼梯";
+            this.Text = "生成楼梯深化模型 V2.0";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox Cmb_Section;
+        private System.Windows.Forms.Label beam_section_text;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox Cmb_stair_type;
     }
 }
 
--- Tekla楼梯生成/TeklaStair/CreateStair.cs
+++ Tekla楼梯生成/TeklaStair/CreateStair.cs
@@ -22,6 +22,7 @@ using System.Security.Cryptography;
 using Tekla.Structures.Geometry3d;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using Point = Tekla.Structures.Geometry3d.Point;
 
 namespace TeklaStair
 {
@@ -29,12 +30,55 @@ namespace TeklaStair
     {
         public CreateStair()
         {
+            
             InitializeComponent();
+            Cmb_stair_type.SelectedIndex =0 ;
+            Cmb_Section.DataSource = GetHProfiles();
+            Cmb_Section.SelectedItem = "HN300*150*6.5*9";
+
+
+
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        public static List<string> GetHProfiles()
+        {
+            List<Profile> hProfiles = new List<Profile>();
+            //LibraryProfileItem correntProfileItem = null;
+            //List<LibraryProfileItem> profileL = new List<LibraryProfileItem>();
+            List<String> profileL_Name = new List<string>();
+            CatalogHandler CatalogHandler = new CatalogHandler();
+            ProfileItemEnumerator ProfileItemEnumerator = CatalogHandler.GetLibraryProfileItems();
+
+
+            while (ProfileItemEnumerator.MoveNext())
+            {
+                LibraryProfileItem LibraryProfileItem = ProfileItemEnumerator.Current as LibraryProfileItem;
+
+                string section_name = LibraryProfileItem.ProfileName;
+                if (section_name.StartsWith("H"))
+                    profileL_Name.Add(section_name); //将选中构件的截面汇入截面库profileL
+
+            }
+
+
+            //for (int j = 0; j < profileL.Count; j++)
+            //{
+            //    if (profileL[j].ProfileName.Contains(profileName))
+            //    {
+            //        correntProfileItem = profileL[j];//在截面库profileL中历遍寻找当截面的信息
+            //    }
+            //}
+            return profileL_Name;
+
+
+        }
+
+
+        public void button1_Click(object sender, EventArgs e)
         {
             string DbPath = @" Y:\数字化课题\数据库\RevitData.db";
+            string section_name_string = Cmb_Section.SelectedItem.ToString();
+            
             using (SQLiteConnection conn = new SQLiteConnection("Data Source = " + DbPath))
             {
                 conn.Open();
@@ -168,11 +212,14 @@ namespace TeklaStair
                     }
                     else if (plate_name == "钢梁中心线")
                     {
-                        Generate_Beam(point_info,myModelPlate);
+                        Generate_Beam(point_info, section_name_string,myModelPlate);
                     }
                     else if (plate_name == "承接钢板")
                     {
-                        Generate_fold_Plate(point_info, plate_thick, myModelPlate);
+                        if(Convert.ToDouble(point_info.Split('*')[0])>100)
+                            Generate_fold_Plate(point_info, plate_thick, myModelPlate);
+                        else
+                            Generate_Plate(point_info, plate_thick, myModelPlate);
                     }
                     else
                     {
@@ -272,31 +319,34 @@ namespace TeklaStair
         private static void Generate_fold_Plate(string point_info,string plate_thick, Model myModelPlate)
         {
             string p_len = point_info.Split('*')[0];
-            string L1 = point_info.Split('*')[1];
-            string L2 = point_info.Split('*')[2];
 
+            List < string >L_s= point_info.Split('*').ToList();
 
-            L1 = L1.Substring(1, L1.Length-2);
-            L1.Replace(" , ", "@");
+            string L1 = L_s[1];
+            string L2 = L_s[2];
+
+            
+            L1 = L1.Substring(1, L1.Length-2).Replace(" , ", "@");
             //L2 = L2.Substring(1, L2.Length-2);
             
             
-            string P1_s = L1.Split(' ')[0];
-            string P2_s = L1.Split(' ')[2];
-            string P3_s = L1.Split(' ')[4];
-           
 
-            Point P1 = new Point(new Point(Convert.ToDouble(P1_s.Split(',')[0]), Convert.ToDouble(P1_s.Split(',')[1]),
-                Convert.ToDouble(P1_s.Split(',')[2])));
-            Point P2 = new Point(new Point(Convert.ToDouble(P2_s.Split(',')[0]), Convert.ToDouble(P2_s.Split(',')[1]),
-                Convert.ToDouble(P2_s.Split(',')[2])));
-            Point P3 = new Point(new Point(Convert.ToDouble(P3_s.Split(',')[0]), Convert.ToDouble(P3_s.Split(',')[1]),
-                Convert.ToDouble(P3_s.Split(',')[2])));
+            List<string> P_s = L1.Split('@').ToList();
+            List<Point> PXYZ_s = new List<Point>();
+
+            for (int i = 0; i < P_s.Count; i++)
+            {
+                double x_coord = Convert.ToDouble(P_s[i].Split(',')[0]);
+                double y_coord = Convert.ToDouble(P_s[i].Split(',')[1]);
+                double z_coord = Convert.ToDouble(P_s[i].Split(',')[2]);
+                PXYZ_s.Add(new Point(x_coord, y_coord, z_coord));
+            }
 
 
-            ContourPoint point1 = new ContourPoint(P1,null);
-            ContourPoint point2 = new ContourPoint(P2, null);
-            ContourPoint point3 = new ContourPoint(P3,null);
+
+            ContourPoint point1 = new ContourPoint(PXYZ_s[0],null);
+            ContourPoint point2 = new ContourPoint(PXYZ_s[1], null);
+            ContourPoint point3 = new ContourPoint(PXYZ_s[2], null);
 
             PolyBeam PolyBeam = new PolyBeam();
 
@@ -314,22 +364,10 @@ namespace TeklaStair
             Result = PolyBeam.Insert();
 
 
-            // 创建一个新的PolyLine对象，并将其点位置设置为myPoints列表
-
-            //SingleRebar.OnPlaneOffsets = new ArrayList();
-            //SingleRebar.OnPlaneOffsets.Add(25.00);
-            //SingleRebar.StartHook.Angle = -90;
-            //SingleRebar.StartHook.Length = 10;
-            //SingleRebar.StartHook.Radius = 10;
-            //SingleRebar.StartHook.Shape = RebarHookData.RebarHookShapeEnum.CUSTOM_HOOK;
-            //SingleRebar.EndHook.Angle = 90;
-            //SingleRebar.EndHook.Length = 10;
-            //SingleRebar.EndHook.Radius = 10;
-            //SingleRebar.EndHook.Shape = RebarHookData.RebarHookShapeEnum.CUSTOM_HOOK;
 
             myModelPlate.CommitChanges();
         }
-        private static void Generate_Beam(string point_info,  Model myModelPlate)
+        private static void Generate_Beam(string point_info,  string section_name_string, Model myModelPlate)
         {
             string ps = point_info.Split('*')[1];
             string pe = point_info.Split('*')[2];
@@ -340,7 +378,10 @@ namespace TeklaStair
                 Convert.ToDouble(pe.Split(',')[2])));
 
             Beam Beam = new Beam(P1, P2);
-            Beam.Profile.ProfileString = "H175*90*5*8";//"300-6-10*200";
+            Beam.Profile.ProfileString = section_name_string;//"300-6-10*200";
+
+
+
             Beam.Material.MaterialString = "Q345B";
             //Beam.Finish = "PAINT";
             Beam.Insert();
@@ -378,5 +419,9 @@ namespace TeklaStair
             return table;
         }
 
+        private void Cmb_stair_type_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
